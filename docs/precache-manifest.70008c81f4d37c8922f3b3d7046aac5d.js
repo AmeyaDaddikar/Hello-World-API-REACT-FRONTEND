8a48dfadc910365ba3460732bde0f4d287c6dbc2
Ameya Daddikar <ameyadaddikar@gmail.com>
2020-01-22 21:25:56 +0530

    Added docs folder for deployment on Github Pages

---
+++ docs/precache-manifest.70008c81f4d37c8922f3b3d7046aac5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b19b0f5c3db6ff2512afea91670059",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/index.html"
+  },
+  {
+    "revision": "5d0e1911bd2c7be122c8",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "89978de72bf98dd0f38b",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "5d0e1911bd2c7be122c8",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/2.45579ef4.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/2.45579ef4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89978de72bf98dd0f38b",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/main.e10efbb1.chunk.js"
+  },
+  {
+    "revision": "867e285f3ce97bb448f6",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/runtime-main.25792b8c.js"
+  },
+  {
+    "revision": "aafd8235a189f83ced5fd3b17a3439e3",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/media/translator.aafd8235.png"
+  }
+]);
