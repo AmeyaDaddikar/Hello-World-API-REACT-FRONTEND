b524aa808d2a42d3bb9a4376a5d7116423f1d883
Ameya Daddikar <ameyadaddikar@gmail.com>
2020-01-22 21:38:52 +0530

    Updates

---
+++ precache-manifest.26f35916808d6f97eeb5f238d86235a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16e883fb3cb5396fd1cba2bf40447b2",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/index.html"
+  },
+  {
+    "revision": "7271f5882fe75c842111",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "f8d852e425cbfb08c789",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7271f5882fe75c842111",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/2.29e51742.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/2.29e51742.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8d852e425cbfb08c789",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/main.7a8af095.chunk.js"
+  },
+  {
+    "revision": "ea66a72ed3f3a76a7b57",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/js/runtime-main.681f51bc.js"
+  },
+  {
+    "revision": "aafd8235a189f83ced5fd3b17a3439e3",
+    "url": "/AmeyaDaddikar/Hello-World-API-REACT-FRONTEND/static/media/translator.aafd8235.png"
+  }
+]);
